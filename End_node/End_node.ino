66f91a2bcf777ba82066c67770949ed9dc5a2e3b
Erdem <erdemcanaz@gmail.com>
2021-11-14 23:44:25 +0300

    .
    .

--- End_node/End_node.ino
+++ End_node/End_node.ino
@@ -1,4 +1,4 @@
-#define TROUBLESHOOTING false //if you want to see th process ongoing, should be turned of while operating
+#define TROUBLESHOOTING true //if you want to see th process ongoing, should be turned of while operating
 #define RESET_PIN 7 //this pin directly connected to reset pin of arduino
 
 #define HARD_RESET_TIME 90000 // after how much miliseconds should device reset itself
@@ -7,6 +7,7 @@ unsigned long DEL_RESET_TIME; //to be sure that arduino reset time differs.
 
 #define BROADCAST_DELAY 2000 // how much miliseconds should pass to broadcast the new data
 
+float objects[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 struct msg {
   unsigned long SENDER_ID = 0;
   unsigned long DESTINATION_ID = 0;
@@ -17,20 +18,25 @@ struct msg {
 struct msg serialMsg;
 
 void setup() {
-  digitalWrite(RESET_PIN,HIGH);//after defining a pin output, by default it is LOW. Thus if you put this after "pinMode(RESET_PIN,OUTPUT);", device continuously resets itself.
+  digitalWrite(RESET_PIN, HIGH); //after defining a pin output, by default it is LOW. Thus if you put this after "pinMode(RESET_PIN,OUTPUT);", device continuously resets itself.
   Serial.begin(9600);
-  pinMode(RESET_PIN,OUTPUT);
+  pinMode(RESET_PIN, OUTPUT);
   DEL_RESET_TIME = random(DEL_RESET_MAX);
 }
 
 void loop() {
-  ///
-  if(millis()>HARD_RESET_TIME+DEL_RESET_TIME)digitalWrite(RESET_PIN,LOW);
+  ////////// DO NOT MESS WITH THIS BLOCK
+  if (millis() > HARD_RESET_TIME + DEL_RESET_TIME)digitalWrite(RESET_PIN, LOW);
   ///
   boolean isSerialMsgReceived = read_serial();
-  if(isSerialMsgReceived){
-      void reply_it();
+  if (isSerialMsgReceived) {
+    if (TROUBLESHOOTING)Serial.println("Serial mesajı alinmis");
+    reply_it();
   }
+  ///////----------------------------------------
+
+  //write code that sets or gets values from values[];
+
 
 }
 
@@ -38,8 +44,47 @@ unsigned long values[] = {0, 0, 0, 0, 0, 0, 0}; // [0]:SENDER_ID, [1]:DESTINATIO
 boolean isSerialLocked = true;
 unsigned long last_time_broadcast = 0;
 
-void reply_it(){
-  Serial.println("#1,2,3,4,5.0$");
+void reply_it() {
+  if (TROUBLESHOOTING)Serial.println("Serial mesajina cevap veriyorum");
+  //float objects[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  serialMsg.DESTINATION_ID = serialMsg.SENDER_ID;
+  serialMsg.SENDER_ID = 0; //does not matter, LoRa will change it just before broadcasting
+  int task = serialMsg.TASK ; //0:set, 1:learn, 2 reply;
+  serialMsg.TASK = 2;
+  int object = serialMsg.WHICH_OBJECT;
+
+  if (task == 0) { //set WHICH_OBJECT to FLOAT_VALUE
+    if (object > 2 && object < 6) { //3,4,5
+      values[WHICH_OBJECT] = serialMsg.FLOAT_VALUE;
+      if (TROUBLESHOOTING)Serial.println(String(object) + ". obje " + String(serialMsg.FLOAT_VALUE) + " degerine set edildi.");
+      Serial.print("#" + String(serialMsg.SENDER_ID));
+      Serial.print("," + String(serialMsg.DESTINATION_ID));
+      Serial.print("," + String(serialMsg.TASK));
+      Serial.print("," + String(serialMsg.WHICH_OBJECT));
+      Serial.print("," + String(serialMsg.FLOAT_VALUE, 5));
+      Serial.println("$");
+      return;
+      //send reply;
+    } else {
+      if (TROUBLESHOOTING)Serial.println(String(object) + ". obje set edilemez");
+      return;
+    }
+  } else if (task == 1) { //send WHICH_OBJECT to master
+    if (object >= 0 && object < 3) { //0,1,2
+      serialMsg.FLOAT_VALUE = values[object];
+      if (TROUBLESHOOTING)Serial.println(String(object) + ". objenin degeri olan " + String(values[object]) + " gönderilecek");
+      Serial.print("#" + String(serialMsg.SENDER_ID));
+      Serial.print("," + String(serialMsg.DESTINATION_ID));
+      Serial.print("," + String(serialMsg.TASK));
+      Serial.print("," + String(serialMsg.WHICH_OBJECT));
+      Serial.print("," + String(serialMsg.FLOAT_VALUE, 5));
+      Serial.println("$");
+      return;
+    } else {
+      if (TROUBLESHOOTING)Serial.println(String(object) + ". objenin degeri öğrenilemez.");
+      return;
+    }
+  }  
 }
 boolean read_serial() {
   //#: start $:end ;:other variable .:other variable
@@ -80,7 +125,7 @@ boolean read_serial() {
     while (k > 1)k = k / 10.0f;
     serialMsg.FLOAT_VALUE = values[4] + k;
 
-    if (TROUBLESHOOTING)print_msg(true,false);
+    if (TROUBLESHOOTING)print_msg(true, false);
 
     for (int i = 0 ; i < 7; i ++)values[i] = 0;
     isSerialLocked = true;
@@ -101,8 +146,8 @@ boolean read_serial() {
   }
   return false;
 }
-void print_msg(boolean isSerial,boolean isFormatted) {
-  if(isFormatted){
+void print_msg(boolean isSerial, boolean isFormatted) {
+  if (isFormatted) {
     if (isSerial) {
       Serial.print("#" + String(serialMsg.SENDER_ID));
       Serial.print("," + String(serialMsg.DESTINATION_ID));
@@ -113,16 +158,16 @@ void print_msg(boolean isSerial,boolean isFormatted) {
     }
     else {
     }
-  }else{
-  if (isSerial) {
-    Serial.print("SerialMsg //:");
-    Serial.print(" SENDER_ID:" + String(serialMsg.SENDER_ID));
-    Serial.print(" DESTINATION_ID:" + String(serialMsg.DESTINATION_ID));
-    Serial.print(" TASK:" + String(serialMsg.TASK));
-    Serial.print(" WHICH_OBJECT:" + String(serialMsg.WHICH_OBJECT));
-    Serial.println(" FLOAT_VALUE:" + String(serialMsg.FLOAT_VALUE, 5));
+  } else {
+    if (isSerial) {
+      Serial.print("SerialMsg //:");
+      Serial.print(" SENDER_ID:" + String(serialMsg.SENDER_ID));
+      Serial.print(" DESTINATION_ID:" + String(serialMsg.DESTINATION_ID));
+      Serial.print(" TASK:" + String(serialMsg.TASK));
+      Serial.print(" WHICH_OBJECT:" + String(serialMsg.WHICH_OBJECT));
+      Serial.println(" FLOAT_VALUE:" + String(serialMsg.FLOAT_VALUE, 5));
+    }
+    else {
+    }
   }
-  else {
-  }
-}
 }
